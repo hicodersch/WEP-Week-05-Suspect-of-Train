5d0a3d60b35d071dc6b8617ef76076aba5b39ab6
dali1870 <62158766+dali1870@users.noreply.github.com>
2020-12-11 22:50:04 +0100

    Update javaweek3.js

--- javaweek3.js
+++ javaweek3.js
@@ -1,11 +1,13 @@
-/*  -Math random nesnesi ile 1-10 arası rastgele numarası uretılır ve supheli bu vagonda saklanacaktır
-    -Supheli cabbari bulmak icin Kullanıcıdan vagon numarası gireceği değeri almak icin promp kullanılacaktır
-    -if else dongusu ile yukarı aşagı diyerek dogru sonuca ulaştırılması sağlanacaktır */
+/*Program akisi:
+1.  1-10 arasinda rastgele vagon numarası uretilir ve suclu bu vagonda saklanması sağlanır.
+2.  Kullanıcıya sucluyu bulmak icin 4 hak verilir.
+3.  Suclunun kolay bulunabilmasi icin yönlendirme ifadeleri kullanilir.(Ön Vagon-Arka Vagon)
+4.  Suclunun kacinci denemede bulundugu ve aldigi puan alert ile ekrana yazdirilir. */
 
 const toplamhak = 4;
 // Math.random()*10 1 ile 9 arası sayı üretir bundan dolayı +1 eklendi
 const saklanilanVagon = Math.floor((Math.random() * 10) + 1); //Şüphelinin Saklandıgı vagon 
-for (let kalanhak = toplamhak, sayac = 1; kalanhak >= 0; kalanhak--, sayac++) { //kullanıcıya verilen tahmin hakkı
+for (let kalanhak = toplamhak, sayac = 1; kalanhak >= 0; kalanhak--, sayac++) { //kullanıcıya verilen tahmin hakkına göre for döngüsü calışacaktır
     let tahmin = Number(prompt('vagon numarası giriniz'));
     if (saklanilanVagon == tahmin) {
         alert(`Tebrikler Süpheliyi ${sayac} defada hangi vagonda oldugunu bildiniz`);
